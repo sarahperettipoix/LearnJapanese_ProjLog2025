0330ac5f84bdaecb45a01fcfaba09ad1729bd6ef
Orsoladee <102214276+Orsoladee@users.noreply.github.com>
2025-04-09 17:14:59 +0200

    liaison tout ca

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,15 @@ import json
 from dataclasses import dataclass, field
 from fastapi import Response, FastAPI, HTTPException
 from user import *
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import random
 
 app = FastAPI()
+templates = Jinja2Templates(directory="../frontend")
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
 
 @dataclass
 class Kanji:
@@ -26,24 +33,27 @@ with open("db/kanjis.json", encoding="utf8") as file:
 
 @dataclass
 class Hiragana:
-    id: str
+    id: int
+    kana: str
     romaji: str
 
-hiraganas: dict[str, Hiragana] = {}
+hiraganas: dict[int, str, Hiragana] = {}
 
 #put in file path to JSON
 with open("db/hiragana.json", encoding="utf8") as file:
     hiraganas_raw = json.load(file)
+    #uses line as separartion to iterate on text file (hiragana raws)
     for hiragana_raw in hiraganas_raw:
         hiragana = Hiragana(**hiragana_raw)
         hiraganas[hiragana.id] = hiragana
 
 @dataclass
 class Katakana:
-    id: str
+    id: int
+    kana: str
     romaji: str
 
-katakanas: dict[str, Katakana] = {}
+katakanas: dict[int, str, Katakana] = {}
 
 #put in file path to JSON
 with open("db/katakana.json", encoding="utf8") as file:
@@ -52,43 +62,95 @@ with open("db/katakana.json", encoding="utf8") as file:
         katakana = Katakana(**katakana_raw)
         katakanas[katakana.id] = katakana
 
-@app.get("/") #ask server to get sthg for u
-def read_root() ->Response:
-    return Response("The server is running.")
+@app.get("/index.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def home(request: Request):
+    """return server running"""
+    message = "Bienvenue sur ma page avec FastAPI et Jinja2!"
+    return templates.TemplateResponse("index.html", {"request": request, "message": message})
+
+@app.get("/kanji.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def home(request: Request):
+    """return server running"""
+    message = "Bienvenue sur l'apprentissage des kanjis!"
+    return templates.TemplateResponse("kanji.html", {"request": request, "message": message})
+
+@app.get("/flashcard.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def flashcard(request: Request):
+    """return server running"""
+    message = "Bienvenue sur les flashcards!"
+    return templates.TemplateResponse("flashcard.html", {"request": request, "message": message})
+
+@app.get("/browse.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def browse(request: Request):
+    """return server running"""
+    message = "Bienvenue sur dans la partie browse!"
+    return templates.TemplateResponse("browse.html", {"request": request, "message": message})
+
+@app.get("/learn.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def learn(request: Request):
+    """return server running"""
+    message = "Bienvenue sur dans la partie browse!"
+    return templates.TemplateResponse("learn.html", {"request": request, "message": message})
+
+@app.get("/about.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def about(request: Request):
+    """return server running"""
+    message = "Bienvenue sur nos informations!"
+    return templates.TemplateResponse("about.html", {"request": request, "message": message})
+
+@app.get("/login.html", response_class=HTMLResponse) #ask server to get sthg for u
+async def login(request: Request):
+    """return server running"""
+    message = "Bienvenue sur la page de connexion!"
+    return templates.TemplateResponse("login.html", {"request": request, "message": message})
 
 
 #what frontend will have to do
 @app.get("/kanji/{kanji_id}", response_model=Kanji)
 def read_kanji(kanji_id: int) -> Kanji:
+    """return knajis based on kanji id"""
     if kanji_id not in kanjis:
         raise HTTPException(status_code=404, detail="Kanji not found")
     return kanjis[kanji_id]
 
-@app.get("/hiragana/{hiragana_id}", response_model=Hiragana)
-def read_hiragana(hiragana_id: str) -> Hiragana:
-    if hiragana_id not in hiraganas:
-        raise HTTPException(status_code=404, detail="Hiragana not found")
-    return hiraganas[hiragana_id]
+
+
+@app.get("/hiragana", response_class=HTMLResponse)
+async def read_hiragana(request: Request):
+    """return hiraganas based on hiragana id"""
+
+    random_list = []
+    for ninin in len(hiraganas):
+        random_list[ninin].append(random.randint(1,len(hiraganas)))
+    print(random_list)
+
+    message = "Bienvenue sur ma page avec FastAPI et Jinja2!"
+    return templates.TemplateResponse("flashcard.html", {"request": request, "message": message})
+
+
 
 @app.get("/katakana/{katakana_id}", response_model=Katakana)
 def read_katakana(katakana_id: str) -> Katakana:
+    """return katakanas based on katakana id"""
     if katakana_id not in katakanas:
         raise HTTPException(status_code=404, detail="Katakana not found")
     return katakanas[katakana_id]
 
 
 @app.get("/exists/{username}", response_model=str)
-def read_username(username: str):
-    u = User()
-    if u.username_exists(username):
-        return "valid username", username
+def read_username(username: str) ->Response:
+    """return username if username exist."""
+    u = User(username=username)
+    if u.username_exists():
+        return Response("valid username: " + username)
     raise HTTPException(status_code=404, detail="Username not found")
 
-
-"""@app.get("/hello/{name}")
-def read_hello(name: str) ->Response:
-    return Response("hello " + name)
-
-@app.get("/hello")
-def read_hello() ->Response:
-    return Response("hello world")"""
+#post for user sending info
+@app.post("/user/add", response_model=User)
+def user_add(user: User) -> User:
+    """add new user."""
+    try:
+        user.add()
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"{e}") #returns error message of ValueError of user.py
+    return user
--- runBackend.bash
+++ runBackend.bash
@@ -1,2 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bashpip
 cd backend && uvicorn main:app --host 0.0.0.0 --port 8080 --reload
+<<<<<<< Updated upstream
+=======
+#ouvrir le terminal taper:  ./runBackend.bash
+#ouvrir le browser taper localhost:8080/ (suivi du path que vous voulez voir)
+#terminer le programme sur le terminal avec ctrl c
+
+>>>>>>> Stashed changes
