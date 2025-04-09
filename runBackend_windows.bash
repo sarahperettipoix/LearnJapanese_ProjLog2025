2115fb2b2fcb8ba60768b8b836a946da6e0fa51c
Virgile Albasini <161448772+valbasin@users.noreply.github.com>
2025-04-09 22:19:08 +0200

    d

--- runBackend_windows.bash
+++ runBackend_windows.bash
@@ -7,17 +7,3 @@ python -m uvicorn main:app --host 0.0.0.0 --port 8080 --reload
 
 #ouvrir le browser taper localhost:8080/ (suivi du path que vous voulez voir)
 #terminer le programme sur le terminal avec ctrl c
-
-
-################# POUR MONGO DB ##############
-#dans un terminal
-mongod --dbpath C:\0_TRAVAIL\1_UNI\3eme\z_creation_logiciel\ProjetLogiciel2025\backend\db
-
-# pour le tuer 
-net stop MongoDB
-
-# pour le lancer
-net start MongoDB
-
-# pour vérifier son état
-netstat -ano | findstr 27017
