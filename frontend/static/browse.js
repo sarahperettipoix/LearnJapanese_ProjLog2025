57e382221aef376d7998a48a3dd0b38cff189c78
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2025-05-14 17:22:45 +0200

    browse styling

--- frontend/static/browse.js
+++ frontend/static/browse.js
@@ -28,7 +28,7 @@ dropdowns.forEach(dropdown => {
             if( selected.innerText == "Hiragana"){
                 boddy.innerHTML = DATA.hiragana.map(item => `
                     <div class="card">
-                        <p>${item.kana}  ${item.romaji}</p>
+                        <p>/* ${item.kana}  */ ${item.romaji}</p>
                     </div>
                 `).join("");
             }
@@ -42,7 +42,9 @@ dropdowns.forEach(dropdown => {
             else if(selected.innerText == "Kanji"){
                 boddy.innerHTML = DATA.kanji.map(item => `
                     <div class="card">
-                        <p>${item.JLPT} ${item.kanji} ${item.onyomi} ${item.kunyomi} ${item.meaning} </p>
+                        ${item.kanji}
+                        <div style="font-size: 30px"> (${item.onyomi}${item.kunyomi})</div>
+                        <div style="color: darkgreen; font-style: italic; font-size: 30px">${item.meaning}</div>
                     </div>
                 `).join("");
             }
